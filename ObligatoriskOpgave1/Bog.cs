f3d1726f9e28b29e9354f37bf20043cf2bac6b83
Oliver Eierstrand <oliv1115@edu.easj.dk>
2020-02-27 09:27:50 +0100

    Oprettet bog class med properties

--- ObligatoriskOpgave1/Bog.cs
+++ ObligatoriskOpgave1/Bog.cs
@@ -4,5 +4,40 @@ namespace ObligatoriskOpgave1
 {
     public class Bog
     {
+        private string _title;
+        private string _forfatter;
+        private int _sidetal;
+        private int _isbn;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string Forfatter
+        {
+            get { return _forfatter; }
+            set { _forfatter = value; }
+        }
+        public int Sidetal
+        {
+            get { return _sidetal; }
+            set { _sidetal = value; }
+        }
+
+        public int ISBN
+        {
+            get { return _isbn; }
+            set { _isbn = value; }
+        }
+
+        public Bog(string title, string forfatter, int sidetal, int isbn)
+        {
+            _title = title;
+            _forfatter = forfatter;
+            _sidetal = sidetal;
+            _isbn = isbn;
+        }
+
     }
 }
