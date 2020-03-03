fc43fb2a296f762f5f360f362969ee790acd481a
Oliver Eierstrand <oliv1115@edu.easj.dk>
2020-03-03 13:46:08 +0100

    Opgave 1 - C# og Unit test Done

---
+++ ObligatoriskOpgave1/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+
+namespace ObligatoriskOpgave1
+{
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private int _noOfPages;
+        private string _isbn13;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value == null) throw new NullReferenceException();
+                _title = value;
+            }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                if (value == null) throw new NullReferenceException();
+                if (value.Length < 2) throw new ArgumentOutOfRangeException();
+                _title = value;
+            }
+        }
+
+        public int NoOfPages
+        {
+            get { return _noOfPages; }
+            set
+            {
+                if (value <= 4) throw new ArgumentOutOfRangeException();
+                if (value >= 1000) throw new ArgumentOutOfRangeException();
+                _noOfPages = value;
+            }
+        }
+
+        public string ISBN13
+        {
+            get { return _isbn13; }
+            set
+            {
+                _isbn13 = value;
+                if (value.Length == 13) _isbn13 = value;
+                else if (value.Length != 13) throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public Book(string title, string author, int noOfPages, string isbn13)
+        {
+            _title = title;
+            _author = author;
+            _noOfPages = noOfPages;
+            _isbn13 = isbn13;
+        }
+    }
+}
---
+++ ObligatoriskOpgave1BogTests/BookTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOpgave1;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace ObligatoriskOpgave1.BogTests
+{
+    //####################### 
+    //### 81% CODE COVERAGE ###
+    //#######################
+
+    [TestClass()]
+    public class BookTests
+    {
+        private Book _book;
+
+        [TestInitialize]
+        public void BookTest()
+        {
+            _book = new Book("Løvernes Konge","Karl Marx",100,"1234567891011");
+        }
+        
+        [TestMethod()]
+        public void BookTitleTest()
+        {
+            Assert.AreEqual("Løvernes Konge", _book.Title);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BookTitleTestNullException()
+        {
+            _book.Title = null;
+        }
+
+        [TestMethod()]
+        public void BookAuthorTest()
+        {
+            Assert.AreEqual("Karl Marx", _book.Author);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void BookAuthorTestNullException()
+        {
+            _book.Author = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BookAuthorTestOutOfRangeException()
+        {
+            _book.Author = "a";
+        }
+
+        [TestMethod]
+        public void BookNoOfPagesTest()
+        {
+            Assert.AreEqual(100,_book.NoOfPages);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BookNoOfPagesOutOfRangeLowerException()
+        {
+            _book.NoOfPages = 3;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BookNoOfPagesOutOfRangeHigherException()
+        {
+            _book.NoOfPages = 1001;
+        }
+
+        [TestMethod]
+        public void BookISBNTest()
+        {
+            _book.ISBN13 = "1234567891011";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BookISBNOutOfRangeLowerException()
+        {
+            _book.ISBN13 = "123456789";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BookISBNOutOfRangeHigherException()
+        {
+            _book.ISBN13 = "123456789101112131416";
+        }
+    }
+}
